1b2e986174e85c16a0b53d26553d97318b62844a
some-earth11 <sk8762@srmist.edu.in>
2022-10-21 00:39:35 +0530

    added resources

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (  
+        <div>
+            <nav>
+                <div>
+                <a href="#">
+                    <img src="earthcartoonicon_1.png" alt="img"/>
+                    <div>
+                        Navbar
+                    </div>
+                </a>
+                </div>
+            </nav>            
+        </div>
+    );
+}
+ 
+export default Navbar;
