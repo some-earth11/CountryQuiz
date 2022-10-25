3a1987d902e0f6e5a541141316fe29a38e81405e
some-earth11 <sk8762@srmist.edu.in>
2022-10-25 21:13:39 +0530

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ const Footer = () => {
     return (  
         <div className='footer'>
             <div className="footertext">
-                Made with <FavoriteIcon sx={{ fontSize: 18 }}/> and <CoffeeIcon/> by SOMEEARTH
+                Made with <FavoriteIcon sx={{ fontSize: 13 }}/> and <CoffeeIcon sx={{ fontSize: 15 }}/> by
+                <a href="https://github.com/some-earth11" target="_blank" rel="noreferrer">some-earth</a>
             </div>
         </div>
     );
